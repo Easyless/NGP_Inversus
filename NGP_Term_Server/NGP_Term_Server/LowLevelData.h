1573c101b5fdb97266d2b2229233751fe9f4cba9
KHJ <kimhj06160@gmail.com>
2020-11-21 22:19:50 +0900

    서버 로우레벨 데이터 수정

--- NGP_Term_Server/NGP_Term_Server/LowLevelData.h
+++ NGP_Term_Server/NGP_Term_Server/LowLevelData.h
@@ -1,17 +1,20 @@
-#pragma comment(lib, "ws2_32")
-#define _WINSOCK_DEPRECATED_NO_WARNINGS 1
-#include <winsock2.h>
-#include <utility>
-
-#pragma pack
+#pragma once
+#include <Windows.h>
 #define MAX_PLAYER_LENGTH 4
 #define MAX_LIFE_COUNT 3
-#define BLOCK_COUNT_X 19 // 단위 갯수, 맵에서 블럭 가로,세로 개수
-#define BLOCK_COUNT_Y 19 // 단위 갯수, 맵에서 블럭 가로,세로 개수
-#define BLOCK_SIZE_X 50 //단위 px , 블럭의 픽셀 크기
-#define BLOCK_SIZE_Y 50 //단위 px , 블럭의 픽셀 크기
+#define BLOCK_COUNT_X 21 // 11.21 수정 단위 갯수, 맵에서 블럭 가로,세로 개수
+#define BLOCK_COUNT_Y 21 // 11.21 수정 단위 갯수, 맵에서 블럭 가로,세로 개수
+#define BLOCK_SIZE_X 40 // 11.21 수정 단위 px , 블럭의 픽셀 크기
+#define BLOCK_SIZE_Y 40 // 11.21 수정 단위 px , 블럭의 픽셀 크기
+#define BULLET_SIZE_X 8 //11.21 추가 : 총알 사이즈 
+#define BULLET_SIZE_Y 8 //11.21 추가 : 총알 사이즈 
+#define PLAYER_SIZE_X 25 //11.21 추가 : 플레이어, 적 사이즈
+#define PLAYER_SIZE_Y 25 //11.21 추가 : 플레이어, 적 사이즈
+#define EXPLOSION_SIZE 3 //11.21 추가 : 폭발 블럭 갯수 (3x3) 블럭
+#define EXPLOSION_SIZE 3 //11.21 추가 : 폭발 블럭 갯수 (3x3) 블럭
 #define MAP_SIZE_X (BLOCK_COUNT_X * BLOCK_SIZE_X) //단위 px, 맵 전체 픽셀 크기
 #define MAP_SIZE_Y (BLOCK_COUNT_Y * BLOCK_SIZE_Y) //단위 px, 맵 전체 픽셀 크기
+#define SERVER_PORT 15073
 
 enum NetGameMessageType : unsigned char
 {
@@ -33,7 +36,9 @@ enum NetGameMessageType : unsigned char
 	MSG_EVENT_EXPLOSION = 9,	// S->C 파라메터 있음 (개수 가변)
 	MSG_EVENT_SPAWN = 10,		// S->C 파라메터 있음 (개수 가변)
 
-	MSG_PLAYER_INPUT = 11		// C->S 파라메터 있음 (단일)
+	MSG_PLAYER_INPUT = 11,		// C->S 파라메터 있음 (단일)
+
+	MSG_GAME_END = 12
 };
 
 
@@ -121,9 +126,6 @@ struct NetGameMessage
 	UINT parameterSize; //뒤에 따라올 메시지 후속 정보의 크기 (갯수 * 구조체 사이즈)
 };
 
-
-#pragma pack()
-
 //Utill Function
 inline UINT GetMessageParameterSize( NetGameMessageType type )
 {
@@ -135,11 +137,31 @@ inline UINT GetMessageParameterSize( NetGameMessageType type )
 	case MSG_REQ_READY: return 0;
 	case MSG_CANCLE_READY: return 0;
 	case MSG_GAME_START: return 0;
+	case MSG_GAME_END: return 0;
 	case MSG_SCENE_DATA: return sizeof( GameSceneData );
 	case MSG_BULLET_DATA: return sizeof( BulletData );
 	case MSG_MOB_DATA: return sizeof( MobData );
 	case MSG_PLAYER_INPUT: return sizeof( PlayerInput );
+	}
+}
 
+#define TOSTRING(x) #x
+
+inline const char* GetMessageString( NetGameMessageType type )
+{
+	switch ( type )
+	{
+	case MSG_MESSAGE_NULL: return TOSTRING( MSG_MESSAGE_NULL );
+	case MSG_WAIT_ROOM_DATA: return TOSTRING( MSG_WAIT_ROOM_DATA );
+	case MSG_GAME_ALL_READY: return TOSTRING( MSG_GAME_ALL_READY );
+	case MSG_REQ_READY: return TOSTRING( MSG_REQ_READY );
+	case MSG_CANCLE_READY: return TOSTRING( MSG_CANCLE_READY );
+	case MSG_GAME_START: return TOSTRING( MSG_GAME_START );
+	case MSG_GAME_END: return TOSTRING( MSG_GAME_END );
+	case MSG_SCENE_DATA: return TOSTRING( MSG_SCENE_DATA );
+	case MSG_BULLET_DATA: return TOSTRING( MSG_BULLET_DATA );
+	case MSG_MOB_DATA: return TOSTRING( MSG_MOB_DATA );
+	case MSG_PLAYER_INPUT: return TOSTRING( MSG_PLAYER_INPUT );
 	}
 }
 
@@ -153,6 +175,7 @@ inline UINT GetMessageParameterCount( NetGameMessageType type, UINT parameterSiz
 	case MSG_REQ_READY: return 0;
 	case MSG_CANCLE_READY: return 0;
 	case MSG_GAME_START: return 0;
+	case MSG_GAME_END: return 0;
 	case MSG_SCENE_DATA: return parameterSize / sizeof( GameSceneData );
 	case MSG_BULLET_DATA: return parameterSize / sizeof( BulletData );
 	case MSG_MOB_DATA: return parameterSize / sizeof( MobData );
